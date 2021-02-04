6650bc7b21ace8889135e40f212cf5388f0638d0
dongasai <1514582970@qq.com>
2021-02-04 14:44:50 +0800

    init

--- test/FirstTest.php
+++ test/FirstTest.php
@@ -1,5 +1,8 @@
 <?php
 
+/**
+ * 
+ */
 class FirstTest extends \PHPUnit\Framework\TestCase
 {
 
@@ -22,6 +25,11 @@ class FirstTest extends \PHPUnit\Framework\TestCase
         $this->assertGreaterThan(self::$time[0], $re3->time);
         // 组数据为旧数据
         $this->assertEquals(self::$time['userteam_1'], $re3->team->time);
+        // 同一个组，时间一样
+        $this->assertEquals(self::$time['userteam_1'], $user1->get([5])->team->time);
+//        、、 不同的组，无缓存，时间最新
+        
+        $this->assertGreaterThan(self::$time['userteam_1'], $user1->get([2])->team->time);
     }
 
     public function test2()
@@ -32,8 +40,16 @@ class FirstTest extends \PHPUnit\Framework\TestCase
         $team->update();
         $user1 = new test\Cache\User1([1]);
         $reUser= $user1->get();
-        // 
+        $newTime = $reUser->team->time;
+        // 会关联更新，组成员数据，时间最新
         $this->assertGreaterThan(self::$time['userteam_1'], $reUser->team->time);
+        $this->assertGreaterThan(self::$time['userteam_1'], $user1->get([5])->team->time);
+        // 非组成员，不更新，时间滞后
+        $this->assertLessThan($newTime, $user1->get([2])->team->time);
+        sleep(1);
+        // 全新的组
+        $user4 = $user1->get([4]);
+        $this->assertGreaterThan($newTime, $user4->team->time);
     }
 
 }
